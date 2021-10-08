e1bb213c9c7ea3a20e8dd74028203e4a3a468f76
aljoharah21 <aljoharah.ali.ruh@tuwaiq.edu.sa>
2021-10-09 00:13:55 +0300

    my home work

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+function myFirstFunction(childNum, PartnerName, gLocation,job ) {
+    console.log("you will be a "+ job +" in "+ gLocation + " and married to " + PartnerName + " with "+ childNum + " kids. ")
+   
+};
+
+
+function myAge(currentyear,birthyear){
+    console.log("They are years old "+(currentyear-birthyear))
+
+}
+
+
+function  Geometrizer (radius) {
+    let circumfrance= 2*3.14*radius
+    let area = 3.14*radius^2
+console.log(area + "and " + circumfrance)
+}
+
+
+
+
+
+
+
+
